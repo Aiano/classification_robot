d788e4e718c4b5268579d5e79e6bbc6e16d9d612
Aiano_czm <2293995622@qq.com>
2020-01-23 23:37:58 +0800

    change main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <opencv2/opencv.hpp>
 #include "preprocess.h"
 
+#define CAMERA_ID 0
+int main(){
+        cv::VideoCapture cap(0);
+        if(!cap.isOpened()){
+                std::cerr<<"Can't open camera."<<std::endl;
+                return -1;
+        }
 
-int mian(){
+        cv::Mat frame,backGround,result;
+        while(true){
+                cap>>frame;
 
+                cv::imshow("Frame",frame);
+                int key=cv::waitKey(23);
+                if(key==27) break;
+                else if(key=='b'){
+                        backGround=frame.clone();
+                        cv::imshow("Background",backGround);
+                        cv::waitKey(0);
+                        cv::destroyWindow("Background");
+                } else if (key=='p'){
+                        process(frame,backGround,result);
+                        cv::imshow("Result",result);
+                        cv::waitKey(0);
+                        cv::destroyWindow("Result");
+                        drawDivision(result,result);
+                        cv::imshow("Result",result);
+                        cv::waitKey(0);
+                        cv::destroyWindow("Result");
+                }
+        }
         return 0;
 }
